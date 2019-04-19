740d3503d6cdda92492cbc412ee82fd102025640
err0rzzz <s190988@uos.ac.uk>
2019-04-19 18:55:16 +0100

    built scene and layout, UI, sorting layers, started FSM for agents

---
+++ Interactive Agents/Interactive Agents/Assets/Scripts/Agent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public enum State {Idle, Mission, Training, Moving}
+    public int intel, dex, str;
+
+    public GameObject ActiveMission;
+
+    
+}
